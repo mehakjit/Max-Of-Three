39a68db95079d949e5e4870251cb1fa116c3e989
mehakjit <mehakjitsingh@gmail.com>
2020-10-10 15:34:14 +0530

    [Mehakjit] Added: Function to find maximum Integer value

--- src/main/java/com/capg/TestMaxOfThree.java
+++ src/main/java/com/capg/TestMaxOfThree.java
@@ -1,7 +1,22 @@
 package com.capg;
 
+import java.util.Scanner;
+
 public class TestMaxOfThree {
+	
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome : Compute max of three program");
+		System.out.println("Enter the three no to find out which is maximum");
+		Integer a = sc.nextInt() , b= sc.nextInt(), c = sc.nextInt();
+		TestMaxOfThree maximum = new TestMaxOfThree();
+		System.out.println(maximum.maxOfThree(a, b , c));		
+	}
+
+	public Integer maxOfThree(Integer a, Integer b, Integer c) {
+		Integer max = (a.compareTo(b) > 0) ?  a : b ;
+		max = (max.compareTo(c) > 0) ? max : c ;
+		return max;
 	}
 }
--- src/test/java/com/capg/TestMaxFunc.java
+++ src/test/java/com/capg/TestMaxFunc.java
@@ -1,4 +1,22 @@
 package com.capg;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMaxFunc {
+	@Test
+	public void if_FirstElement_Max_ReturnTrue() {
+		TestMaxOfThree maximum = new TestMaxOfThree();
+		Assert.assertTrue(((Integer)5).equals(maximum.maxOfThree(5, 4, 3)));
+	}
+	@Test
+	public void if_SecondElement_Max_ReturnTrue() {
+		TestMaxOfThree maximum = new TestMaxOfThree();
+		Assert.assertTrue(((Integer)5).equals(maximum.maxOfThree(4, 5, 3)));
+	}
+	@Test
+	public void if_ThirdElement_Max_ReturnTrue() {
+		TestMaxOfThree maximum = new TestMaxOfThree();
+		Assert.assertTrue(((Integer)5).equals(maximum.maxOfThree(3, 4, 5)));
+	}
 }
