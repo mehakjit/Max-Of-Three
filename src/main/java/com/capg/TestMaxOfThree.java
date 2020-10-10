bb7352dbae199632d4f65aa822ed710bec92a231
mehakjit <mehakjitsingh@gmail.com>
2020-10-10 09:00:37 +0530

    [Mehakjit] Added: Welcome Message

---
+++ src/main/java/com/capg/TestMaxOfThree.java
@@ -0,0 +1,7 @@
+package com.capg;
+
+public class TestMaxOfThree {
+	public static void main(String[] args) {
+		System.out.println("Welcome : Compute max of three program");
+	}
+}
---
+++ src/test/java/com/capg/TestMaxFunc.java
@@ -0,0 +1,4 @@
+package com.capg;
+
+public class TestMaxFunc {
+}
