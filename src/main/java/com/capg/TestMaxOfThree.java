c89db500d2adc5c3b52337d72a9ec5d3dede5730
mehakjit <mehakjitsingh@gmail.com>
2020-10-13 07:21:00 +0530

    [Mehakjit] Created : Function to find the max of three Float Values

--- src/main/java/com/capg/TestMaxOfThree.java
+++ src/main/java/com/capg/TestMaxOfThree.java
@@ -11,11 +11,20 @@ public class TestMaxOfThree {
 		System.out.println("Enter the three no to find out which is maximum");
 		Integer a = sc.nextInt() , b= sc.nextInt(), c = sc.nextInt();
 		TestMaxOfThree maximum = new TestMaxOfThree();
-		System.out.println(maximum.maxOfThree(a, b , c));		
+		System.out.println(maximum.maxOfThree(a, b , c));
+		System.out.println("Enter the three float no to find out which is maximum");
+		Float l = sc.nextFloat(), m = sc.nextFloat(), n = sc.nextFloat();
+		System.out.println(maximum.maxOfThree(l, m , n));
 	}
 
 	public Integer maxOfThree(Integer a, Integer b, Integer c) {
 		Integer max = (a.compareTo(b) > 0) ?  a : b ;
+		max = (max.compareTo(c) > 0) ? max : c ;
+		return max;
+	}
+	
+	public Float maxOfThree(Float a, Float b, Float c) {
+		Float max = (a.compareTo(b) > 0) ?  a : b ;
 		max = (max.compareTo(c) > 0) ? max : c ;
 		return max;
 	}
