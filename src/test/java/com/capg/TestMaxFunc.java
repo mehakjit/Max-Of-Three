3b100507d8d15af86ab8013f11a0391d839bf7f2
mehakjit <mehakjitsingh@gmail.com>
2020-10-14 07:37:02 +0530

    [Mehakjit] Added: Max method to take more than three parameters

--- src/test/java/com/capg/TestMaxFunc.java
+++ src/test/java/com/capg/TestMaxFunc.java
@@ -5,48 +5,17 @@ import org.junit.Test;
 
 public class TestMaxFunc {
 	@Test
-	public void ifFirst_Int_Element_Max_ReturnTrue() {
-		TestMaxOfThree<Integer> maximum = new TestMaxOfThree<Integer>(5,4,3);
-		Assert.assertTrue(((Integer)5).equals(maximum.maxOfThree()));
+	public void testInteger() {
+		TestMaxOfThree<Integer> test = new TestMaxOfThree<Integer>(1,2,5,3,4);
+		Assert.assertTrue(((Integer)5).equals(test.max()));
 	}
 	@Test
-	public void ifSecond_Int_Element_Max_ReturnTrue() {
-		TestMaxOfThree<Integer> maximum = new TestMaxOfThree<Integer>(4,5,3);
-		Assert.assertTrue(((Integer)5).equals(maximum.maxOfThree()));
+	public void testFloat() {
+		TestMaxOfThree<Float> test = new TestMaxOfThree<Float>(1f,2f,5f,3f,4f,0f);
+		Assert.assertTrue(((Float)5f).equals(test.max()));
 	}
-	@Test
-	public void ifThird_Int_Element_Max_ReturnTrue() {
-		TestMaxOfThree<Integer> maximum = new TestMaxOfThree<Integer>(3,4,5);
-		Assert.assertTrue(((Integer)5).equals(maximum.maxOfThree()));
-	}
-	@Test
-	public void ifFirst_Float_Element_Max_ReturnTrue() {
-		TestMaxOfThree<Float> maximum = new TestMaxOfThree<Float>(5f,4f,3f);
-		Assert.assertTrue(((Float)5.0f).equals(maximum.maxOfThree()));
-	}
-	@Test
-	public void ifSecond_Float_Element_Max_ReturnTrue() {
-		TestMaxOfThree<Float> maximum = new TestMaxOfThree<Float>(4f,5f,3f);
-		Assert.assertTrue(((Float)5f).equals(maximum.maxOfThree()));
-	}
-	@Test
-	public void ifThird_Float_Element_Max_ReturnTrue() {
-		TestMaxOfThree<Float> maximum = new TestMaxOfThree<Float>(4f,3f,5f);
-		Assert.assertTrue(((Float)5f).equals(maximum.maxOfThree()));
-	}
-	@Test
-	public void ifFirst_String_Element_Max_ReturnTrue() {
-		TestMaxOfThree<String> maximum = new TestMaxOfThree<String>("abc","ab","a");
-		Assert.assertTrue(("abc").equals(maximum.maxOfThree()));
-	}
-	@Test
-	public void ifSecond_String_Element_Max_ReturnTrue() {
-		TestMaxOfThree<String> maximum = new TestMaxOfThree<String>("ab","abc","a");
-		Assert.assertTrue(("abc").equals(maximum.maxOfThree()));
-	}
-	@Test
-	public void ifThird_String_Element_Max_ReturnTrue() {
-		TestMaxOfThree<String> maximum = new TestMaxOfThree<String>("ab","a","abc");
-		Assert.assertTrue(("abc").equals(maximum.maxOfThree()));
+	public void testString() {
+		TestMaxOfThree<String> test = new TestMaxOfThree<String>("a","ab","abcde","abcd");
+		Assert.assertTrue(("abcde").equals(test.max()));
 	}
 }
